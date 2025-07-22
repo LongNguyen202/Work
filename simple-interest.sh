505d1e30a3a431ff051356ca7f0d9b52b2a6f4b0
Nguyễn Bảo Long <longnbhe187086@fpt.edu.vn>
2025-07-22 20:35:02 +0700

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Simple Interest Calculator
+
+echo "Enter Principal (P):"
+read p
+echo "Enter Rate of Interest (R):"
+read r
+echo "Enter Time (T):"
+read t
+
+si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+echo "Simple Interest = $si"
